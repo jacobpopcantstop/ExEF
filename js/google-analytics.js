8b80b3c3ac5f98a743316523fd03e0dcfbd29b97
jacobpopcantstop <jacobansky@gmail.com>
2026-05-14 15:27:54 -0700

    Add analytics tag and separate Brain Mode quiz

---
+++ js/google-analytics.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-2WLZPV0PD6');
+})();
